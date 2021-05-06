f5c4e1a36ca1af652a267a2a82c574778a875725
johnfn <johnfn@gmail.com>
2021-05-06 09:55:08 -0700

    Redundancy

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Team Juicebox Games</title>
+        <title>Team Juicebox</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Team Juicebox Games
+          Team Juicebox
         </h1>
 
         <p className={styles.description}>
